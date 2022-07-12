df1f964832cec9af25eeb829bb8c4f7fdf2c36ad
carineribeirost <70036092+carineribeirost@users.noreply.github.com>
2022-07-12 11:43:16 -0300

    Update molDynamics.sh

--- molDynamics.sh
+++ molDynamics.sh
@@ -4,14 +4,18 @@
 #and put them together in a folder of a selected forcefield
 #then run the molecular dynamics calculations
 
-mkdir charm
-cp (*.pdb,ions.mdp,md.mdp,em.mdp,npt.mdp,nvt.mdp,ie.mdp) charm
-cp (md_charm36.sh, charmm36-jul2021.ff.tgz, jz4.str, jz4.prm) charm
-cp (edit_topol.py, edit_topol2.py, combine_pl.py, cgenff_charmm2gmx_py3_nx2.py) charm
+mkdir charmm
+cp (md_charmm36.sh) charmm
+cd files
+cp (*.pdb,ions.mdp,md.mdp,em.mdp,npt.mdp,nvt.mdp,ie.mdp,charmm36-jul2021.ff.tgz, jz4.str, jz4.prm) $(pwd)/../charmm
+cd ..
+cd scripts
+cp (edit_topol.py, edit_topol2.py, combine_pl.py, cgenff_charmm2gmx_py3_nx2.py) $(pwd)/../charmm
+cd ..
 
-cd charm
+cd charmm
 
 tar charmm36-jul2021.ff.tgz
 
-md_charm36.sh
+md_charmm36.sh
 
